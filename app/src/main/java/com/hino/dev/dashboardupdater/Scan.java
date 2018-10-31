4fe34ca04fa259058e164f9c286b73b0fcd4bc15
kevin <contact.kevs@gmail.com>
2018-10-31 20:14:23 +0800

    temporary disabled emdk

--- app/src/main/java/com/hino/dev/dashboardupdater/Scan.java
+++ app/src/main/java/com/hino/dev/dashboardupdater/Scan.java
@@ -11,26 +11,27 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.symbol.emdk.EMDKManager;
-import com.symbol.emdk.EMDKResults;
-import com.symbol.emdk.barcode.BarcodeManager;
-import com.symbol.emdk.barcode.ScanDataCollection;
-import com.symbol.emdk.barcode.Scanner;
-import com.symbol.emdk.barcode.ScannerException;
-import com.symbol.emdk.barcode.ScannerResults;
+//import com.symbol.emdk.EMDKManager;
+//import com.symbol.emdk.EMDKResults;
+//import com.symbol.emdk.barcode.BarcodeManager;
+//import com.symbol.emdk.barcode.ScanDataCollection;
+//import com.symbol.emdk.barcode.Scanner;
+//import com.symbol.emdk.barcode.ScannerException;
+//import com.symbol.emdk.barcode.ScannerResults;
 
 import java.util.ArrayList;
 
-public class Scan extends AppCompatActivity implements EMDKManager.EMDKListener, Scanner.DataListener {
+public class Scan extends AppCompatActivity {
+//public class Scan extends AppCompatActivity implements EMDKManager.EMDKListener, Scanner.DataListener {
 
 
     private ImageView btn_back;
     private ImageView btn_scan;
     private EditText txt_chassisNumber;
     private TextView lbl_scanner_status;
-    private EMDKManager emdkManager;
-    private BarcodeManager barcodeManager;
-    private Scanner scanner;
+//    private EMDKManager emdkManager;
+//    private BarcodeManager barcodeManager;
+//    private Scanner scanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,15 +47,15 @@ public class Scan extends AppCompatActivity implements EMDKManager.EMDKListener,
         txt_chassisNumber = findViewById(R.id.txt_chassisNumber);
         lbl_scanner_status = findViewById(R.id.lbl_scanner_status);
 
-        EMDKResults emdkResults = EMDKManager.getEMDKManager(getApplicationContext(),this);
-        if(emdkResults.statusCode != EMDKResults.STATUS_CODE.SUCCESS){
-            lbl_scanner_status.setText("SCANNER FAILED TO INITIALIZE!");
-        }
+//        EMDKResults emdkResults = EMDKManager.getEMDKManager(getApplicationContext(),this);
+//        if(emdkResults.statusCode != EMDKResults.STATUS_CODE.SUCCESS){
+//            lbl_scanner_status.setText("SCANNER FAILED TO INITIALIZE!");
+//        }
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                releaseScanner();
+//                releaseScanner();
                 finish();
             }
         });
@@ -62,94 +63,94 @@ public class Scan extends AppCompatActivity implements EMDKManager.EMDKListener,
 
     }
 
-    @Override
-    public void onOpened(EMDKManager emdkManager) {
-        this.emdkManager = emdkManager;
-
-        try {
-            initScanner();
-        } catch (ScannerException e) {
-            e.printStackTrace();
-        }
-        lbl_scanner_status.setText("");
-    }
-
-    @Override
-    public void onClosed() {
-        releaseScanner();
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        releaseScanner();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        releaseScanner();
-        finish();
-    }
-
-    private void initScanner() throws ScannerException {
-        if(scanner == null){
-            barcodeManager = (BarcodeManager) this.emdkManager.getInstance(EMDKManager.FEATURE_TYPE.BARCODE);
-            scanner = barcodeManager.getDevice(BarcodeManager.DeviceIdentifier.DEFAULT);
-            scanner.addDataListener(this);
-            scanner.triggerType = Scanner.TriggerType.SOFT_ALWAYS;
-            scanner.enable();
-            scanner.read();
-        }
-    }
-
-    @Override
-    public void onData(ScanDataCollection scanDataCollection) {
-        new AsyncDataUpdate().execute(scanDataCollection);
-    }
-
-    private class AsyncDataUpdate extends AsyncTask<ScanDataCollection,Void,String>{
-
-        @Override
-        protected String doInBackground(ScanDataCollection... scanDataCollections) {
-            String barcodeValue = "";
-            ScanDataCollection scanCollection = scanDataCollections[0];
-
-            if(scanCollection != null && scanCollection.getResult() == ScannerResults.SUCCESS){
-                ArrayList<ScanDataCollection.ScanData> dataArray = scanCollection.getScanData();
-
-                for(ScanDataCollection.ScanData data : dataArray){
-                    barcodeValue = data.getData();
-                }
-            }
-            return barcodeValue;
-        }
-
-        @Override
-        protected void onPostExecute(String barcodeValue) {
-            txt_chassisNumber.setText(barcodeValue);
-            releaseScanner();
-            Intent intent = new Intent(getApplicationContext(),MOPreviewLoading.class);
-            intent.putExtra("chassisNumber",barcodeValue);
-            startActivity(intent);
-            finish();
-        }
-    }
-
-    private void releaseScanner(){
-        if(emdkManager != null){
-            emdkManager.release();
-            emdkManager = null;
-        }
-
-        try{
-            if(scanner != null){
-                scanner.removeDataListener(this);
-                scanner.disable();
-                scanner = null;
-            }
-        } catch (ScannerException e) {
-            e.printStackTrace();
-        }
-    }
+//    @Override
+//    public void onOpened(EMDKManager emdkManager) {
+//        this.emdkManager = emdkManager;
+//
+//        try {
+//            initScanner();
+//        } catch (ScannerException e) {
+//            e.printStackTrace();
+//        }
+//        lbl_scanner_status.setText("");
+//    }
+//
+//    @Override
+//    public void onClosed() {
+//        releaseScanner();
+//    }
+//
+//    @Override
+//    public void onBackPressed() {
+//        super.onBackPressed();
+//        releaseScanner();
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        releaseScanner();
+//        finish();
+//    }
+//
+//    private void initScanner() throws ScannerException {
+//        if(scanner == null){
+//            barcodeManager = (BarcodeManager) this.emdkManager.getInstance(EMDKManager.FEATURE_TYPE.BARCODE);
+//            scanner = barcodeManager.getDevice(BarcodeManager.DeviceIdentifier.DEFAULT);
+//            scanner.addDataListener(this);
+//            scanner.triggerType = Scanner.TriggerType.SOFT_ALWAYS;
+//            scanner.enable();
+//            scanner.read();
+//        }
+//    }
+//
+//    @Override
+//    public void onData(ScanDataCollection scanDataCollection) {
+//        new AsyncDataUpdate().execute(scanDataCollection);
+//    }
+//
+//    private class AsyncDataUpdate extends AsyncTask<ScanDataCollection,Void,String>{
+//
+//        @Override
+//        protected String doInBackground(ScanDataCollection... scanDataCollections) {
+//            String barcodeValue = "";
+//            ScanDataCollection scanCollection = scanDataCollections[0];
+//
+//            if(scanCollection != null && scanCollection.getResult() == ScannerResults.SUCCESS){
+//                ArrayList<ScanDataCollection.ScanData> dataArray = scanCollection.getScanData();
+//
+//                for(ScanDataCollection.ScanData data : dataArray){
+//                    barcodeValue = data.getData();
+//                }
+//            }
+//            return barcodeValue;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String barcodeValue) {
+//            txt_chassisNumber.setText(barcodeValue);
+//            releaseScanner();
+//            Intent intent = new Intent(getApplicationContext(),MOPreviewLoading.class);
+//            intent.putExtra("chassisNumber",barcodeValue);
+//            startActivity(intent);
+//            finish();
+//        }
+//    }
+//
+//    private void releaseScanner(){
+//        if(emdkManager != null){
+//            emdkManager.release();
+//            emdkManager = null;
+//        }
+//
+//        try{
+//            if(scanner != null){
+//                scanner.removeDataListener(this);
+//                scanner.disable();
+//                scanner = null;
+//            }
+//        } catch (ScannerException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
